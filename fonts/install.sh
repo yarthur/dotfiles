85de4fc519bc4c2092ef1006f4b128d16dcb101a
John Arthur <john@jtarthur.net>
2016-07-11 14:52:52 -0500

    Proper install script for new fonts.

---
+++ fonts/install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo -e "\n\nFonts"
+echo "=============================="
+
+cp ~/.dotfiles/fonts/nerd-fonts/patched-fonts/Inconsolata/complete/Inconsolata\ for\ Powerline\ Nerd\ Font\ Complete.otf ~/Library/Fonts
+cp ~/.dotfiles/fonts/nerd-fonts/patched-fonts/Inconsolata/complete/Inconsolata\ for\ Powerline\ Nerd\ Font\ Complete\ Mono.otf ~/Library/Fonts
