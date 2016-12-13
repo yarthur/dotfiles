bc1492187c86d95d680f18f9d56cd3428af42de8
John Arthur <john@jtarthur.net>
2016-12-12 20:01:07 -0600

    Improved integrations for Neovim.

--- nvim/install.sh
+++ nvim/install.sh
@@ -5,13 +5,18 @@ echo -e "=============================="
 
 if test ! $(which nvim); then
 	echo -e "Installing Neovim"
-	brew install neovim/neovim/neovim
+	brew install neovim/neovim/neovim python python3
+	pip2 install neovim
+	pip3 install neovim
+	gem install neovim
 
 	if [ ! -d ~/.config ]; then
 		mkdir ~/.config
 	fi
 
 	ln -s ~/.dotfiles/nvim ~/.config/nvim
+else
+	brew upgrade neovim
 fi
 
 echo -e "Installing/Upgrading Plugins"
