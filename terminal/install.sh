42c310f201a05cde86c2d733622c04d810f43353
John Arthur <john@jtarthur.net>
2020-06-30 21:26:18 -0500

    update terminal settings

--- terminal/install.sh
+++ terminal/install.sh
@@ -4,7 +4,15 @@ if [ "$separator" = "" ]; then
 	source "$(dirname "$0")"/../lib/env.sh
 fi
 
-infocmp $TERM | sed 's/kbs=^[hH]/kbs=\\177/' > $TERM.ti
-tic $TERM.ti
+infocmp $TERM | sed 's/kbs=^[hH]/kbs=\\177/' > $DOTFILES_HOME/terminal/$TERM.terminfo
+tic $TERM.terminfo
 
-tic $DOTFILES_HOME/terminal/xterm-256color-italic.terminfo
+tic -o $HOME/.terminfo $DOTFILES_HOME/terminal/xterm-256color-italic.terminfo
+
+file=$DOTFILES_HOME/terminal/Profiles.json 
+target=$HOME/Library/Application\ Support/iTerm2/DynamicProfiles/Profiles.json
+
+if (! [ -h "$target" ]); then
+	echo "Creating link for YarthTerm"
+	ln -s $file "$target"
+fi
