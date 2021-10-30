f78d957048fcca6a22d110c49aa7c01e2ffbf8e3
John Arthur <john@jtarthur.net>
2021-10-30 01:06:20 -0500

    improve terminal color stuff

--- terminal/install.sh
+++ terminal/install.sh
@@ -7,7 +7,7 @@ fi
 infocmp $TERM | sed 's/kbs=^[hH]/kbs=\\177/' > $DOTFILES_HOME/terminal/$TERM.terminfo
 tic $TERM.terminfo
 
-tic -o $HOME/.terminfo $DOTFILES_HOME/terminal/xterm-256color-italic.terminfo
+tic -o $HOME/.terminfo $DOTFILES_HOME/terminal/xterm-256color.terminfo
 
 file=$DOTFILES_HOME/terminal/Profiles.json 
 target=$HOME/Library/Application\ Support/iTerm2/DynamicProfiles/Profiles.json
